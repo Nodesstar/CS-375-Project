74822f1de92d2e80b020cebf76c4a5225de0ac9c
Nodesstar <asao2781@gmail.com>
2022-11-19 14:42:29 -0500

    Created useful functions for filtering

---
+++ app/public/functions.js
@@ -0,0 +1,35 @@
+//takes a tag and a list of recipes as arguments
+
+//recipe.health label
+function filter(recipes, filter) {
+    let filtered = [];
+    for (let recipe of recipes) {
+        let labels = recipe.healthLabels;
+        for (let label of labels) {
+            if (label === filter) {
+                filtered.push(recipe);
+            }
+        }
+    }
+}
+
+//calculates how many more ingredients are needed compared to what is in the pantry
+function ingredient_disparity(recipes, ingredient_list) {
+    for (let recipe of recipes) {
+        let required_ingredients = [];
+        for (let recipe_ingredient of recipe.ingredients) {
+            let disparity = recipe.ingredients.length();
+            let initial = disparity;
+            for (let ingredient of ingredient_list) {
+                if (recipe_ingredient === ingredient) {
+                    disparity -= 1;
+                } 
+            }
+            if (disparity === initial) {
+                required_ingredients.push(recipe_ingredient);
+            }
+        }
+        recipe["disparity"] = disparity;
+        recipe["shopping_list"] = required_ingredients;
+    }
+}
